f86fbfbc8a5b4fb16116f5ca6cb659c5b073c3a5
rhennig <rhennigg@gmail.com>
2022-04-07 21:34:58 -0300

    Ajuste no tempo de iluminação

--- js.js
+++ js.js
@@ -21,7 +21,7 @@ let shuffleOrder = () => {
 }
 
 function lightcolor(element, number) {
-    number = number * 500;
+    number = number * 300;
     setTimeout(()=> {
         element.classList.add('selected');
     }, number-250);
@@ -92,9 +92,5 @@ green.onclick = () => click(0);
 red.onclick = () => click(1);
 yellow.onclick = () => click(2);
 blue.onclick = () => click(3);
-// green.addEventListener('click', click(0));
-// red.addEventListener('click', click(1));
-// yellow.addEventListener('click', click(2));
-// blue.addEventListener('click', click(3));
 
 playGame();
